db5e89e1022906e4b144406864e4d453102c1617
vponce <vponce@wujitouch.com>
2023-02-01 10:38:49 -0500

    Added go redirect

--- server.go
+++ server.go
@@ -25,28 +25,24 @@ func main() {
 		database = getEnvVar("DATABASE_NAME")
 	)
 
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", host, port, username, password, database)
+	if getEnvVar("GIN_MODE") != "release" {
+		psqlconn = psqlconn + " sslmode=disable"
+	}
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	// close database
+	defer db.Close()
+
   router := gin.Default()
 	router.Static("/assets", "./assets")
 	router.SetFuncMap(template.FuncMap{
 		"mod": mod,
 	})
 	router.LoadHTMLGlob("templates/*")
-  router.GET("/", func(c *gin.Context) {
-
-		
-		// connection string
-    psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", host, port, username, password, database)
-		if getEnvVar("GIN_MODE") != "release" {
-			psqlconn = psqlconn + " sslmode=disable"
-		}
-		
-		// open database
-    db, err := sql.Open("postgres", psqlconn)
-    CheckError(err)
-		
-    // close database
-    defer db.Close()
-		
+  router.GET("/", func(c *gin.Context) {		
 		searchText := c.Query("q")
 		page := c.Query("page")
 
@@ -54,11 +50,11 @@ func main() {
 		sqlCount := ""
 		// sql := " "
 		if searchText == "" {
-			sql = `select title, url, preview_url, tags, asset_created_on from crawl_asset order by id desc LIMIT 30`
+			sql = `select id, title, url, preview_url, tags, asset_created_on from crawl_asset order by id desc LIMIT 30`
 			sqlCount = `select count(*) from crawl_asset`
 		} else {
 			ftsPhrase := "'" + strings.Replace(searchText, " ", " <-> ", 10) + "'"
-			sql = fmt.Sprintf(`SELECT title, url, preview_url, tags, asset_created_on FROM crawl_asset WHERE document_vectors @@ to_tsquery(%s) order by id desc  LIMIT 30`, ftsPhrase)
+			sql = fmt.Sprintf(`SELECT id, title, url, preview_url, tags, asset_created_on FROM crawl_asset WHERE document_vectors @@ to_tsquery(%s) order by id desc  LIMIT 30`, ftsPhrase)
 			sqlCount = fmt.Sprintf(`SELECT COUNT(*) FROM crawl_asset WHERE document_vectors @@ to_tsquery(%s)`, ftsPhrase)
 		}
 
@@ -91,18 +87,21 @@ func main() {
 		}
 		
 		type Asset struct {
+			Id int
 			Title, Url, PreviewUrl, Tags, CreatedOn string
 		}
 		var assets []Asset
 		for rows.Next() {
 				var title, url, preview_url, tags, asset_created_on string
+				var id int
 		
-				err = rows.Scan(&title, &url, &preview_url, &tags, &asset_created_on)
+				err = rows.Scan(&id, &title, &url, &preview_url, &tags, &asset_created_on)
 				CheckError(err)
 		
 				// fmt.Println(title, url)
 				
 				asset := Asset{
+					Id: id,
 					Title: title,
 					Url: url,
 					PreviewUrl: preview_url,
@@ -127,6 +126,23 @@ func main() {
 		})
 		// c.HTML(http.StatusOK, "index.tmpl", assets)
   })
+
+	router.GET("/go/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		sql := fmt.Sprintf(`SELECT url FROM crawl_asset WHERE id = %s`, id)
+		fmt.Println(sql);
+		
+		var url string
+		row := db.QueryRow(sql)
+		err = row.Scan(&url)
+		if err != nil {
+			c.Redirect(http.StatusFound, "/")
+		} else {
+			c.Redirect(http.StatusFound, url)
+		}
+	})
+
   router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
